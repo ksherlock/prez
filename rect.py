feeb5f260e7f30000551b51e42e43660dc1540b9
Kelvin Sherlock <ksherlock@gmail.com>
2020-08-02 10:43:26 -0400

    add rectangle example and fix a couple rectangle bugs.

--- rect.py
+++ rect.py
@@ -17,7 +17,7 @@ __all__ = ['point', 'rect', 'size']
 
 def all_defined(*args): return args.count(None)==0
 def all_none(*args): return args.count(None)==len(args)
-def is_listy(x): type(x) in (tuple, list)
+def is_listy(x): return type(x) in (tuple, list)
 
 
 def point(*args, x=None, y=None, h=None, v=None):
@@ -50,7 +50,8 @@ def size(*args, height=None, width=None):
 
 def rect(*args,
 	x=None,y=None,height=None,width=None,
-	h1=None,h2=None,v1=None,v2=None):
+	h1=None,h2=None,v1=None,v2=None,
+	top=None,left=None,right=None,bottom=None):
 
 	if len(args) == 4: return args
 
@@ -68,7 +69,11 @@ def rect(*args,
 		if all_defined(x,y,height,width):
 			return (y, x, y + height, x + width)
 		if all_defined(h1,h2,v1,v2):
-			return (v1, h2, v2, h2)
+			return (v1, h1, v2, h2)
+		if all_defined(top,left,bottom,right):
+			return (top, left, bottom, right)
+
+
 
 	raise ValueError("bad rect parameter")
 
